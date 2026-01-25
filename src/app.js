b53db306dbad9804129dc858e4a06dd38dffee93
huzaifalidev <huzaifalikhan48@gmail.com>
2026-01-25 21:25:06 +0500

    fixed the cors

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ["http://localhost:3000", "https://desi-eatry.huzaifali.tech"],
+  origin: ["http://localhost:3000", "https://desi-eatry-dashboard.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true, 
   allowedHeaders: ["Content-Type", "Authorization"],
